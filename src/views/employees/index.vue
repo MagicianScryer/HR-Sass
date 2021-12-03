9bd172da04a300301ee1cc73f04a272047c10dee
蔡齐林 <1369822068@qq.com>
2021-12-03 17:19:23 +0800

    完成了路由模块的管理

---
+++ src/views/employees/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="employees-container">
+    <div class="app-container">
+      <h2>员工</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 员工
+  name: 'Employees'
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/social/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="social-container">
+    <div class="app-container">
+      <h2>社保</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 社保
+  name: 'Social'
+}
+</script>
+
+<style lang="sass" scoped></style>
